8799c0b94aa6169a5f944d58aa8d69af75e73d1b
1543-STACK <141954178+1543-STACK@users.noreply.github.com>
2024-02-06 10:38:36 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
+import React from "react";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <footer>
+          This project was coded by
+          <a
+            href="https://github.com/1543-STACK/my-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Anu Govender{" "}
+          </a>{" "}
+          and is
+          <a
+            href="https://gthub.com/1543-STACK/my-app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            on GitHub
+          </a>{" "}
+          and
+          <a
+            href="https://relaxed-zuccutto-09d782.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
